3931198cc8cead1897d8ea3f23632b566056d4f3
odyflsya <sitiaudreyfalisya8@gmail.com>
2024-04-08 01:18:12 +0700

    Tugas prak PWL

---
+++ app/Models/Dosen.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Dosen extends Model
+{
+    use HasFactory;
+
+    protected $table = 'dosen';
+    protected $primaryKey = 'id_dosen';
+    public $time_stamps = false;
+
+    public function matkul()
+    {
+        return $this->hasOne(mata_kuliah :: class , 'id_dosen');
+    }
+}
---
+++ app/Models/mata_kuliah.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class mata_kuliah extends Model
+{
+    use HasFactory;
+    protected $table = 'mata_kuliah';
+    protected $primaryKey = 'id_matkul';
+    public $time_stamps = false;
+
+    public function dosen()
+    {
+        return $this->belongsTo(Dosen::class);
+    }  
+}
